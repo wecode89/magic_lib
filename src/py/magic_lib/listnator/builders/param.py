e2cf590ac8a911f4a023ab7d9329af7ae23642ba
wecode89 <61945527+wecode89@users.noreply.github.com>
2022-01-18 18:26:08 -0500

    updated params

--- src/py/magic_lib/listnator/builders/param.py
+++ src/py/magic_lib/listnator/builders/param.py
@@ -18,3 +18,29 @@ class ParamBuilder:
                 del params[k]
         return params
 
+
+class MongoBuilder:
+    def __init__(self, params, ignore_keys=[]):
+        self.params = params
+        self.ignore_keys = ignore_keys
+
+    def build(self):
+        if not self.params:
+            params = {}
+
+        # copay params
+        params = copy.copy(self.params)
+
+        # delete ignore keys
+        for k in self.ignore_keys:
+            if k in params:
+                del params[k]
+
+        # handle special cases
+        new_params = {}
+        for k, v in params.items():
+            if k.endswith("__lte"):
+                new_params[k[:-5]] = {'$lte': float(v)}
+            else:
+                new_params[k] = v
+        return new_params
